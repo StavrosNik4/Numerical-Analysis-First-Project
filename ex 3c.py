450b981c1fd122b46f19af8641b1570e0c1ae509
Stavros Nikolaidis <stavniko@csd.auth.gr>
2021-12-29 15:46:15 +0200

    ex 4 finally done

--- ex 3c.py
+++ ex 3c.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def filla(a):
     n = len(a)
     tmp = np.zeros([n, n], float)
@@ -35,39 +34,25 @@ def fillb(b):
 def gauss_seidel(a, b, x):
     n = len(a)
     xi = x.copy()
-    # for loop for 3 times as to calculate x, y , z
     for j in range(n):
-        # temp variable d to store b[j]
-        d = b[j]
-
-        # to calculate respective xi, yi, z
+        t = b[j]
         for i in range(n):
             if j != i:
-                d -= a[j][i] * xi[i]
-        # updating the value of our solution
-        xi[j] = d / a[j][j]
-    # returning our updated solution
+                t -= a[j][i] * xi[i]
+        xi[j] = t / a[j][j]
     return xi
 
-'''
-n = 3
-a = np.array([[10, 8, 8],
-              [8, 10, 8],
-              [8, 8, 10]],float)
-b = np.array([10, 10, 10], float)
 
-'''
 print("Δώσε το n:")
 n = int(input())
 
-b = np.zeros(n, float)
 a = np.zeros([n, n], float)
-
-b = fillb(b)
-a = filla(a)
-
+b = np.zeros(n, float)
 x = np.zeros(n, float)
 xn = np.zeros(n, float)  # for the warning
+
+a = filla(a)
+b = fillb(b)
 
 
 def maxi(m):
@@ -79,14 +64,11 @@ def maxi(m):
     return ma
 
 
-i = 0
 while True:
     xn = gauss_seidel(a, b, x)
-    i = i + 1
     inf = abs(maxi(xn)) - abs(maxi(x))
-    if abs(inf) <= 0.005:
+    if abs(inf) <= 0.00005:
         break
     x = xn
 
-print("Για n = " + str(n) + ": " + str(xn))
-print("Επαναλήψεις: " + str(i))
+print("Η λύση του συστήματος: " + str(xn))
--- ex 4.py
+++ ex 4.py
@@ -39,20 +39,203 @@ print("z: " + str(z))
 
 # ερώτημα 2
 
-#G = G.T
-
 p = np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], float)
 
+# Μέθοδος των δυνάμεων
 for i in range(n):
     p = np.dot(G, p)
     p = (1 /p[0]) * p
 
-print(p)
-
+# Matrix Normalization
 sum = 0
 for i in range(n):
     sum += p[i]
 for i in range(n):
     p[i] /= sum
 
-print(p)
+print("p: ")
+for i in range(n):
+    print("p[" + str(i+1) + "]: " + str(p[i]))
+print("-----------------------------------")
+
+
+# Ερώτημα 3
+
+A[13][9] = 0
+A[1][13] = 1
+A[2][13] = 1
+A[9][13] = 1
+A[10][13] = 1
+
+G = np.zeros([n, n], float)
+
+for i in range(n):
+    for j in range(n):
+        nj = 0
+        for k in range(n):
+            nj += A[j][k]
+        G[i][j] = q / n + (1 - q) * A[j][i] / nj
+
+p = np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], float)
+
+# Μέθοδος των δυνάμεων
+for i in range(n):
+    p = np.dot(G, p)
+    p = (1 /p[0]) * p
+
+# Matrix Normalization
+sum = 0
+for i in range(n):
+    sum += p[i]
+for i in range(n):
+    p[i] /= sum
+
+print("p (for for question 3): ")
+for i in range(n):
+    print("p[" + str(i+1) + "]: " + str(p[i]))
+print("-----------------------------------")
+
+
+# Ερώτημα 4α
+
+q = 0.02
+G = np.zeros([n, n], float)
+
+for i in range(n):
+    for j in range(n):
+        nj = 0
+        for k in range(n):
+            nj += A[j][k]
+        G[i][j] = q / n + (1 - q) * A[j][i] / nj
+
+p = np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], float)
+
+# Μέθοδος των δυνάμεων
+for i in range(n):
+    p = np.dot(G, p)
+    p = (1 /p[0]) * p
+
+# Matrix Normalization
+sum = 0
+for i in range(n):
+    sum += p[i]
+for i in range(n):
+    p[i] /= sum
+
+print("p (with q = 0.02): ")
+for i in range(n):
+    print("p[" + str(i+1) + "]: " + str(p[i]))
+print("-----------------------------------")
+
+
+# Ερώτημα 4β
+
+q = 0.6
+G = np.zeros([n, n], float)
+
+for i in range(n):
+    for j in range(n):
+        nj = 0
+        for k in range(n):
+            nj += A[j][k]
+        G[i][j] = q / n + (1 - q) * A[j][i] / nj
+
+p = np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], float)
+
+# Μέθοδος των δυνάμεων
+for i in range(n):
+    p = np.dot(G, p)
+    p = (1 /p[0]) * p
+
+# Matrix Normalization
+sum = 0
+for i in range(n):
+    sum += p[i]
+for i in range(n):
+    p[i] /= sum
+
+print("p (with q = 0.6): ")
+for i in range(n):
+    print("p[" + str(i+1) + "]: " + str(p[i]))
+print("-----------------------------------")
+
+
+
+# Ερώτημα 5
+
+A[13][9] = 1
+A[1][13] = 0
+A[2][13] = 0
+A[9][13] = 0
+A[10][13] = 0
+
+A[7][10] = 3
+A[11][10] = 3
+
+q = 0.15
+G = np.zeros([n, n], float)
+
+for i in range(n):
+    for j in range(n):
+        nj = 0
+        for k in range(n):
+            nj += A[j][k]
+        G[i][j] = q / n + (1 - q) * A[j][i] / nj
+
+p = np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], float)
+
+# Μέθοδος των δυνάμεων
+for i in range(n):
+    p = np.dot(G, p)
+    p = (1 /p[0]) * p
+
+# Matrix Normalization
+sum = 0
+for i in range(n):
+    sum += p[i]
+for i in range(n):
+    p[i] /= sum
+
+print("p (for question 5): ")
+for i in range(n):
+    print("p[" + str(i+1) + "]: " + str(p[i]))
+print("-----------------------------------")
+
+# Ερώτημα 6
+
+for i in range(n):
+    A[9][i] = 0
+    A[i][9] = 0
+
+G = np.zeros([n, n], float)
+
+for i in range(n):
+    if i == 9:
+        continue
+    for j in range(n):
+        nj = 0
+        if j == 9:
+            continue
+        for k in range(n):
+            if k == 9:
+                continue
+            nj += A[j][k]
+        G[i][j] = q / n + (1 - q) * A[j][i] / nj
+
+p = np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], float)
+
+# Μέθοδος των δυνάμεων
+for i in range(n):
+    p = np.dot(G, p)
+    p = (1 /p[0]) * p
+
+# Matrix Normalization
+sum = 0
+for i in range(n):
+    sum += p[i]
+for i in range(n):
+    p[i] /= sum
+
+print("p (for question 6): ")
+for i in range(n):
+    print("p[" + str(i+1) + "]: " + str(p[i]))
